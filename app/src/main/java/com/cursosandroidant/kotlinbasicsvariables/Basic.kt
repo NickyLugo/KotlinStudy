7c84f3a06271e6cbf978d6d484d509c0caa7b913
LugoNick <133447787+LugoNick@users.noreply.github.com>
2025-04-27 01:23:56 -0600

    Initial commit

---
+++ app/src/main/java/com/cursosandroidant/kotlinbasicsvariables/Basic.kt
@@ -0,0 +1,10 @@
+package com.cursosandroidant.kotlinbasicsvariables
+import androidx.core.math.MathUtils
+
+var username = "Al"
+const val SPECIES = "Human"
+
+fun main(){
+    println("Hello Nick!")
+    
+}
